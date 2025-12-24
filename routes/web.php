832aef431837a44d3d61f535ab53859c3fa28496
KhalidBH <khalid-net2012@LIVE.COM>
2025-12-24 03:32:37 +0100

    Revert "Edit routes"
    This reverts commit 4bb4907f36dad6fbf56e10294f8c4f4ac4847d29.

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::middleware('auth')->get('/auth-test', function() {
-    $user = auth()->user();
-
-    return [
-        'authenticated' => auth()->check(),
-        'user' => $user ? $user->email : 'null',
-        'user_id' => $user ? $user->id : 'null',
-        'session_id' => session()->getId(),
-    ];
-});
