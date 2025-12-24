4bb4907f36dad6fbf56e10294f8c4f4ac4847d29
KhalidBH <khalid-net2012@LIVE.COM>
2025-12-24 02:28:14 +0100

    Edit routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MediaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/media/{id}', [MediaController::class, 'show'])->name('media.show');
 });
